9e26a1bd6785d0645199a9282f33ff69cc29a480
nelsonwm45 <nelsonwm45@gmail.com>
2025-11-20 03:54:26 +0800

    Added comments for wordpress

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -5,9 +5,17 @@ sleep 10
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 	# Download WordPress
+	# --allow-root: WP-CLI strictly forbids running as 'root' by default for security. 
+	#               Since we are inside a Docker container as root, we MUST use this flag to override the block.
 	wp core download --allow-root
 
-	# Create confid using env vars
+	# This connects this container (WordPress) to the Service 1 container (MariaDB).
+	# wp config create: WP-CLI command to generate the config file.
+	# --dbname: The name of the database to create/use (passed from .env).
+	# --dbuser: The database username (passed from .env).
+	# --dbpass: The database password (passed from .env).
+	# --dbhost="mariadb": CRITICAL. This MUST match the 'container_name' or service name of your database 
+	#                     defined in 'docker-compose.yaml'. Localhost (127.0.0.1) would fail because DB is in a different container.
 	wp config create \
 		--dbname="${MYSQL_DATABASE}" \
 		--dbuser="${MYSQL_USER}" \
@@ -25,6 +33,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 		--allow-root
 
 	# Create second wp user
+	# --role=author: Sets permissions. 'author' is a standard non-admin role. (Cannot be 'administrator').
 	wp user create \
 		"${WP_USER}" "${WP_USER_EMAIL}" \
 		--user_pass="${WP_USER_PASSWORD}" \
